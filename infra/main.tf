94132c772939dd5e0470bb69909a022c801330fb
francisco.lucas_dp6 <francisco.lucas_dp6@prestador.globo>
2025-05-04 15:47:31 -0300

    Add BQ module

--- infra/main.tf
+++ infra/main.tf
@@ -23,3 +23,9 @@ module "storage" {
   project_id = var.project_id
   region = var.region
 }
+
+module "bigquery" {
+  source = "./modules/bigquery"
+  project_id = var.project_id
+  region = var.region
+}
